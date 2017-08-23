f4946b53fc5f293a8565e504f8ffc06a3b5a8905
azouziyahya <yahya.azouzi@esprit.tn>
2017-08-23 17:20:48 +0200

    update v1

---
+++ app.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Yahya on 02/08/2017.
+ */
+var express=require('express');
+var bodyParser=require('body-parser');
+var app=express();
+var PORT=process.env.PORT ||3000 ;
+
+var request = require('request');
+var parser = require('rss-parser');
+/*
+Feed.load('http://blog.reklammuvek.com//feeds/posts/default?alt=rss', function(err, rss){
+    myJson=rss;
+
+
+  newRss=new myRss(rss);
+
+    app.get('/',function (req,res) {
+        res.setHeader('content-type', 'application/rss+xml');
+
+        res.send(newRss);
+
+    });
+});
+*/
+//console.log("title : "+myJson);
+
+/*
+parser.parseURL('http://blog.reklammuvek.com//feeds/posts/default?alt=rss', function(err, parsed) {
+    console.log(parsed.feed.title);
+    parsed.feed.entries.forEach(function(entry) {
+        console.log(entry.title + ':' + entry.link);
+    })*
+
+    app.get('/',function (req,res) {
+        res.set('Content-Type', 'application/rss+xml');
+
+        res.send(parsed);
+
+    });
+});
+*/
+var pd = require('pretty-data').pd;
+var url='http://blog.reklammuvek.com//feeds/posts/default?alt=rss';
+request({url:url,xml:true},function (error,response,body) {
+    if(error)
+    {
+        console.log('unable to fetch feed . ');
+    }
+    else
+    {
+     //  console.log(JSON.stringify(body));
+
+
+        app.get('/',function (req,res) {
+             res.set('Content-Type', 'application/rss+xml');
+
+            res.send(pd.xml(body));
+
+        });
+
+    }
+
+
+});
+
+
+app.listen(PORT,function () {
+    console.log("express server started !! ");
+});
